f91fb44e6cc7aacd598c969a0a56c142a2c41203
xysed <xysed2969@yandex.ru>
2022-08-14 16:06:05 +0700

    Fix search bug

--- src/account/services/search.py
+++ src/account/services/search.py
@@ -5,5 +5,9 @@ def find_all(request) -> list[Account]:
     '''Ищет пользователей по нику. Возвращает их список'''
     if request.method == 'POST':
         search = request.POST["search"]
+
+        if not search:
+            return []
+
         return Account.objects.filter(username__contains=search)
     return []
