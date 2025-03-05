59175e79492bf619b43866b1f00d68a9c8e69633
skrepkaq <skrepkaq@gmail.com>
2025-03-05 17:04:21 +0800

    Изменил БД на postgres

--- src/social/settings.py
+++ src/social/settings.py
@@ -85,8 +85,12 @@ ASGI_APPLICATION = 'social.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'vkkiller',
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
     }
 }
 
